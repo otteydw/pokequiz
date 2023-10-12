e61656096ae28247d386ec0daf5a3db23ab018ac
Daniel W. Ottey <dan@bluefrogs.us>
2023-10-12 18:55:50 -0400

    Better

--- pokequiz/helpers.py
+++ pokequiz/helpers.py
@@ -1,47 +1,57 @@
 import random
+
 import pokebase as pb
 from pokebase import cache
+
 cache.API_CACHE
+
 
 def random_pokemon():
     pokemon_id = random.randint(1, 1010)
     mon = pb.pokemon(pokemon_id)
     return mon
 
+
 # def pokemon_types(pokemon_id):
 #    mon = pb.pokemon(pokemon_id)
 #    mon_types = [pokemon_type.type.name for pokemon_type in mon.types]
 #    return mon_types
 
+
 def pokemon_types(pokemon):
-   mon_types = sorted([pokemon_type.type.name for pokemon_type in pokemon.types])
-   return mon_types
+    mon_types = sorted([pokemon_type.type.name for pokemon_type in pokemon.types])
+    return mon_types
+
 
 def pokemon_type_list():
-    pokemon_type_list = sorted([pokemon_type['name'] for pokemon_type in pb.APIResourceList('type') ])
-    pokemon_type_list.remove('shadow')
-    pokemon_type_list.remove('unknown')
+    pokemon_type_list = sorted(
+        [pokemon_type["name"] for pokemon_type in pb.APIResourceList("type")]
+    )
+    pokemon_type_list.remove("shadow")
+    pokemon_type_list.remove("unknown")
     return pokemon_type_list
 
+
 def combine_words(words):
-  """Combines a list of words into a string, using commas and the word "and" to separate the words.
+    """Combines a list of words into a string, using commas and the word "and" to separate the words.
 
-  Args:
-    words: A list of strings.
+    Args:
+      words: A list of strings.
 
-  Returns:
-    A string containing the combined words, using commas and the word "and" to separate the words.
-  """
+    Returns:
+      A string containing the combined words, using commas and the word "and" to separate the words.
+    """
 
-  if len(words) == 1:
-    return words[0]
-  elif len(words) == 2:
-    return f"{words[0]} and {words[1]}"
-  else:
-    return f"{', '.join(words[:-1])}, and {words[-1]}"
+    if len(words) == 1:
+        return words[0]
+    elif len(words) == 2:
+        return f"{words[0]} and {words[1]}"
+    else:
+        return f"{', '.join(words[:-1])}, and {words[-1]}"
+
 
 def simple_pluralize(word_list, singular_string, pluralized_string):
-   if len(word_list) == 1:
-      return singular_string
+    if len(word_list) == 1:
+        return singular_string
 
-   return pluralized_string
+    return pluralized_string
--- pokequiz/pokequiz.py
+++ pokequiz/pokequiz.py
@@ -1,5 +1,6 @@
 import helpers
 
+
 def type_quiz(hints=False):
     pokemon = helpers.random_pokemon()
 
@@ -12,17 +13,20 @@ def type_quiz(hints=False):
     if hints:
         hint_text = f"Hint: it has {len(pokemon_types)} {helpers.simple_pluralize(pokemon_types, 'type','types')}."
     else:
-        hint_text=""
+        hint_text = ""
     print(f"What are the typings for {pokemon_name}? {hint_text}")
     guess = input("Answer? ")
-    guess_list = sorted(guess.lower().replace(' ','').split(","))
+    guess_list = sorted(guess.lower().replace(" ", "").split(","))
     # print(f"Guesses as sorted list: {guess_list}")
 
     if guess_list == pokemon_types:
         print("Correct!")
     else:
-        print(f"Wrong! {pokemon_name} has {helpers.simple_pluralize(pokemon_types, 'typing','typings')} of {helpers.combine_words(pokemon_types)}.")
+        print(
+            f"Wrong! {pokemon_name} has {helpers.simple_pluralize(pokemon_types, 'typing','typings')} of {helpers.combine_words(pokemon_types)}."
+        )
     print()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     type_quiz(hints=True)
